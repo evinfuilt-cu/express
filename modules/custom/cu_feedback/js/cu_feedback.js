1a77115ef5d3c196f136bc63d380e6b2cff49e43
kevincrafts <kevin.crafts@colorado.edu>
2016-08-04 09:28:42 -0600

    fixing issue where feedback form height could be larger than window height, loading form on page load for quicker reaction, changing close icon from down chevron to times FIT-2049

--- modules/custom/cu_feedback/js/cu_feedback.js
+++ modules/custom/cu_feedback/js/cu_feedback.js
@@ -1,8 +1,10 @@
 (function ($) {
-  $(document).ready(function(){ 
+  $(document).ready(function(){
+    cu_feedback_ajax_load();
      $("#cu-feedback a.cu-feedback-link-embed").click(function() {
         $("#cu-feedback-content").slideToggle('fast');
-        $("#cu-feedback a.cu-feedback-link-embed i.fa").toggleClass("fa-chevron-down");
+        $("#cu-feedback a.cu-feedback-link-embed i.fa").toggleClass("fa-chevron-up");
+        $("#cu-feedback a.cu-feedback-link-embed i.fa").toggleClass("fa-times");
         return false;
      });
   });
