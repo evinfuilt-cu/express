0f7c2822a93542059e939ba20819fb64b686277d
kevincrafts <kevin.crafts@colorado.edu>
2017-01-20 09:39:13 -0700

    switching article grids to use bootstrap classes rather then zen grids FIT-2390

--- modules/features/cu_article/templates/bean--article-grid.tpl.php
+++ modules/features/cu_article/templates/bean--article-grid.tpl.php
@@ -1,4 +1,6 @@
 <div class="article-grid clearfix article-grid-<?php print $content['field_article_grid_summary'][0]['#markup']; ?>">
-  <?php print render($content['articles']); ?>
+  <div class="row article-grid-wrapper">
+    <?php print render($content['articles']); ?>
+  </div>
 </div>
 <?php print render($content['field_article_grid_more_link']); ?>
